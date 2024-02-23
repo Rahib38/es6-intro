04d6cc9ff43f55ead0cbfe02f3fe5297b27855fe
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-22 23:08:23 -0800

    create a chain file

---
+++ chain.js
@@ -0,0 +1,16 @@
+const products = {
+    count : 5000,
+    data : [
+        {
+            id: 1,
+            name : 'lenovo laptop',
+            price : 65000
+        },
+        {
+            id: 1,
+            name : 'lenovo laptop',
+            price : 65000
+        }
+    ]
+};
+console.log(products.data[1].price);

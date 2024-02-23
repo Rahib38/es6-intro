56fe4dcc6dea9691a6d22d7625c8e8a678471d5a
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-22 23:35:47 -0800

    added a foreach

---
+++ foreach.js
@@ -0,0 +1,3 @@
+const numbers = [10, 25, 12, 32, 14];
+const doubleIt = numbers.forEach(numbers => numbers * 2);
+console.log(doubleIt);
---
+++ map.js
@@ -0,0 +1,3 @@
+const numbers = [10, 25, 12, 32, 14];
+const doubleIt = numbers.map(numbers => numbers * 2);
+console.log(doubleIt);

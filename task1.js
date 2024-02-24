6ccb9bd2cecf6841fde8fdfa82311909b8d6ba6c
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-23 21:03:42 -0800

    added a task1

---
+++ task1.js
@@ -0,0 +1,13 @@
+// problem 1
+
+// 1 
+// const multiply = (num1, num2, num3) => num1* num2 * num3;
+// console.log(multiply(8, 5, 3));
+
+// 2
+// const sentence = `I am a web developer. I love to code. I love to eat biryani.`
+// console.log(sentence);
+
+// 3
+// const add = (num1 , num2 = 7) => num1 + num2;
+// console.log(add(9, 11));

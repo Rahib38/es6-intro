240af1f103f6d34fef2f1f6b3ea834fba4d47bce
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-25 15:47:03 -0800

    added a filter

--- task2.js
+++ task2.js
@@ -1,10 +1,17 @@
 // problem 1
 
-const arr = [1,3,5,7,9 ];
+// const arr = [1,3,5,7,9 ];
 // for (const array of arr){
 //     const even = array+1;
 //     console.log(even);
 // };
 
-const num = arr.map(p => p+1);
-console.log(num);
+// const num = arr.map(p => p+1);
+// console.log(num);
+
+// filter()
+
+// const arr = [33, 50, 79, 78, 90, 101, 30];
+
+// const elements = arr.filter(given=> given % 10 === 0);
+// console.log(elements);

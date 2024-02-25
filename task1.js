44adbbe6eab63e02e506c245c2e43097c8a2d2f2
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-25 08:59:54 -0800

    added a task2

--- task1.js
+++ task1.js
@@ -11,3 +11,35 @@
 // 3
 // const add = (num1 , num2 = 7) => num1 + num2;
 // console.log(add(9, 11));
+
+// problem 2
+
+// const friends = ['solim', 'mahadi', 'rahman', 'kobir', 'naim'];
+// const newArray = [];
+// const evenFriends = (friends) => {
+//     for (const friend of friends){
+//         if (friend.length % 2 === 0){
+//             newArray.push(friend);
+            
+//         }
+//     }
+//     return newArray;
+// } 
+// console.log(evenFriends(friends));
+
+
+// problem 3
+let sum = 0;
+let i = 0;
+const numbers = [10, 20, 60, 30, 70];
+const elements = (numbers) => {
+    
+    for (const num of numbers){
+      numbers[i] = num * num;
+        sum += numbers[i];
+        i++;
+    }
+    const avg = sum / numbers.length;
+    return avg.toFixed(2);
+}
+console.log(elements(numbers))
---
+++ task2.js
@@ -0,0 +1,10 @@
+// problem 1
+
+const arr = [1,3,5,7,9 ];
+// for (const array of arr){
+//     const even = array+1;
+//     console.log(even);
+// };
+
+const num = arr.map(p => p+1);
+console.log(num);

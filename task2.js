2091210e754334f4a062ab7bf92d1e392529dd05
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-25 17:24:54 -0800

    added a reduce

--- task2.js
+++ task2.js
@@ -15,3 +15,24 @@
 
 // const elements = arr.filter(given=> given % 10 === 0);
 // console.log(elements);
+// const arr = [33, 50, 79, 78, 90, 101, 30];
+
+// const elements = arr.find(given=> given % 10 === 0);
+// console.log(elements);
+
+// array of objects Filter()
+
+const instructor = [
+    {name: 'Nadi', age: 28, position:"Senior"},
+    {name: 'Akil', age: 26, position:"junior"},
+    {name: 'sobuj', age: 30, position:"Senior"}
+];
+// const display = instructor.filter(given=> given.position === "Senior");
+// console.log(display);
+
+
+// reduce()
+
+const sum = instructor.reduce((p, current)=>{
+    return p + current.age},0);
+console.log(sum); 
